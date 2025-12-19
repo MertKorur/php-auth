58aca76f03321c6ace96a9925dcb98fe664233f7
Mert Korur <mrtkorur@gmail.com>
2025-12-19 18:14:56 +0100

    Enhance registration form styling and structure with Bootstrap classes

--- registration.php
+++ registration.php
@@ -5,25 +5,29 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Registration Form</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
     <div class="container">
         <form action="registration.php" method="post">
             <div class="form-group">
-                <input type="text" name="username" placeholder="Username" required>
+                <input type="text" class="form-control" name="username" placeholder="Username" required>
             </div>
             <div class="form-group">
-                <input type="email" name="email" placeholder="Email" required>
+                <input type="email" class="form-control" name="email" placeholder="Email" required>
             </div>
             <div class="form-group">
-                <input type="password" name="password" placeholder="Password" required>
+                <input type="password" class="form-control" name="password" placeholder="Password" required>
             </div>
             <div class="form-group">
-                <input type="password" name="repeat_password" placeholder="Repeat Password" required>
+                <input type="password" class="form-control" name="repeat_password" placeholder="Repeat Password"
+                    required>
             </div>
-            <div class="form-group">
-                <input type="submit" value="Register">
+            <div class="form-btn">
+                <input type="submit" class="btn btn-primary" value="Register">
             </div>
         </form>
     </div>
